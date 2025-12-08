8ae040b8a5ff97a9ae30f583d06a47775877192c
Stanislav <mikhel.sk@gmail.com>
2025-12-08 21:41:19 +0300

    Update database

--- Task3/build_index.py
+++ Task3/build_index.py
@@ -1,18 +1,10 @@
 import os
-import tiktoken
 import chromadb
 from chromadb.utils import embedding_functions
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def chunk_by_tokens(text: str, max_tokens: int = 128, overlap: int = 32, enc=None):
-    enc = enc or tiktoken.get_encoding("cl100k_base")
-    tokens = enc.encode(text)
-    start = 0
-    while start < len(tokens):
-        end = start + max_tokens
-        chunk = enc.decode(tokens[start:end])
-        yield chunk, start
-        start = end - overlap  # шаг назад для overlap
-
+text_splitter = RecursiveCharacterTextSplitter(
+  chunk_size=1024, chunk_overlap=64)
 
 # files to chunks
 texts = []
@@ -23,19 +15,20 @@ for root, directories, files in os.walk(dir_path):
         # read file
         with open(filepath) as f:
             s = f.read()
-            lst = []
-            for ch, pos in chunk_by_tokens(s, 600, 60):
-                lst.append((ch, pos, filepath))
-            texts += lst
+            chunks = text_splitter.split_text(s)
+            n = 0
+            for chunk in chunks:
+                texts.append((chunk, str(n), filepath))
+                n += len(chunk)
 
-
+print("chanks:", len(texts))
 CHROMA_DATA_PATH = "chroma_data/"
-EMBED_MODEL = "all-MiniLM-L6-v2"
 COLLECTION_NAME = "star_wars"
 
-client = chromadb.PersistentClient(path=CHROMA_DATA_PATH)
 embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name=EMBED_MODEL)
+  model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+client = chromadb.PersistentClient(path=CHROMA_DATA_PATH)
 collection = client.create_collection(
     name=COLLECTION_NAME,
     embedding_function=embedding_func,
@@ -46,5 +39,5 @@ chunks = [grp[0] for grp in texts]
 collection.add(
     documents=chunks,
     ids=[f'id{i}' for i in range(len(chunks))],
-    metadatas=[{"pos": p[1], "file": p[2]} for p in chunks]
+    metadatas=[{"pos": p[1], "file": p[2]} for p in texts]
 )
